80b8ca588e88b5ae966cfb12a443152e82251343
Vitalik Levin <s_top_games@vk.com>
2021-11-08 10:37:58 +0300

    0.0.06-08-11-2021
    Changes:
    -New panel in Menu
    -New enemy: Fly

--- Assets/Scripts/Scripts for UI/OtherSettings.cs
+++ Assets/Scripts/Scripts for UI/OtherSettings.cs
@@ -5,10 +5,17 @@ using UnityEngine.UI;
 
 public class OtherSettings : MonoBehaviour 
 {
-	//https://github.com/VitalikLevin/Bounce-s-Adventure.git
+	//https://github.com/VitalikLevin/Bounce-s-Adventure
 	//Fool's day https://clck.ru/9TFat
 	public void SocialNetworks ()
 	{
 		Application.OpenURL ("https://vk.com/");
+	}
+	public GameObject quitPanel;
+	public Transform canvas;
+	public void FakeChoice ()
+	{
+		GameObject newPanel = Instantiate (quitPanel);
+		newPanel.transform.SetParent (canvas);
 	}
 }
