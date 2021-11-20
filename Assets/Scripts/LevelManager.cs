0cd2715abaff0d1e418a0df429fec491d19e630e
Vitalik Levin <s_top_games@vk.com>
2021-11-20 14:48:15 +0300

    0.0.09-20-11-2021
    Changes:
    -Now you can edit the lawn in Unity Editor

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -8,6 +8,10 @@ public class LevelManager : MonoBehaviour
 	private GameObject tile;
 	[SerializeField]
 	private Transform parent;
+	//Size of a lawn
+	public int customX;
+	public int customY;
+	public Vector3 customPos;
 
 	void Start ()
 	{
@@ -17,9 +21,9 @@ public class LevelManager : MonoBehaviour
 	private void CreateLevel ()
 	{
 		float tileSize = tile.GetComponent<SpriteRenderer> ().sprite.bounds.size.x;
-		for (int y = 0; y < 5; y++)
+		for (int y = 0; y < customY; y++)
 		{
-			for (int x = 0; x < 9; x++)
+			for (int x = 0; x < customX; x++)
 			{
 				GameObject newTile = Instantiate (tile);
 				newTile.transform.SetParent (parent);
@@ -29,6 +33,6 @@ public class LevelManager : MonoBehaviour
 	}
 	private void IdealPosition ()
 	{
-		parent.position = new Vector3 (-4.84f, -3.31f, 0);
+		parent.position = new Vector3 (customPos.x, customPos.y, 0);
 	}
 }
