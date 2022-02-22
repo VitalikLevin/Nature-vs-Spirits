134bea86dccb2c01d7e14b1e029fc941a2c0e0ca
Vitalik Levin <network_worms@mail.ru>
2022-02-22 17:22:30 +0300

    0.0.19-22-02-2022
    Added:
    - Pause menu in Level 1
    Changed:
    - UI in Main Menu

---
+++ Assets/Scripts/Scripts for UI/Pause.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+	private bool isPaused;
+	[SerializeField]
+	private GameObject pObj;
+	public void pActive () 
+	{
+		isPaused = !isPaused;
+		pObj.SetActive(isPaused);
+		Time.timeScale = 0.0f;
+	}
+	public void WakeUp ()
+	{
+		Time.timeScale = 1.0f;
+	}
+	public void GoMain ()
+	{
+		SceneManager.LoadScene (0);
+	}
+	void Update () 
+	{
+		if (Input.GetKeyUp(KeyCode.Escape))
+		{
+			pActive ();
+			if (isPaused == true)
+			{
+				WakeUp ();
+			}
+		}
+	}
+}
